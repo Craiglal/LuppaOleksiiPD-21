50d37be21872725bebc43b10a230395383b6c12e
Oleksii Luppa <asleep.alex@gmail.com>
2019-09-09 21:30:53 +0300

    Completed the task

--- 09.09_CSharp/09.09 CSharp/Program.cs
+++ 09.09_CSharp/09.09 CSharp/Program.cs
@@ -6,7 +6,16 @@ namespace _09._09_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double mult = 1.0;
+
+            for (int k = 0; k < 50; k++)
+            {
+                mult *= (((-1) ^ k * k ^ 2 + (-1) ^ (k ^ (2 + 1)) * k) * (2 * k ^ 3 + 6 * k ^ 2 + 6 * k + 5)) / ((2 * k ^ 3 + 3) * (-1) ^ (k + 1) * (k ^ 2 + 2 * k + 1) + (-1) ^ (k ^ 2 + 2) * (k + 1));
+            }
+
+            Console.WriteLine(mult);
         }
     }
 }
+// (((-1) ^ k * k ^ 2 + (-1) ^ (k ^ (2 + 1)) * k) * (2 * k ^ 3 + 6 * k ^ 2 + 6 * k + 5)) / ((2 * k ^ 3 + 3) * (-1) ^ (k + 1) * (k ^ 2 + 2 * k + 1) + (-1) ^ (k ^ 2 + 2) * (k + 1))
+// (Math.Pow(-1, k) * Math.Pow(k, 2) + Math.Pow(-1, Math.Pow(k, 2) + 1) * k) / (2 * Math.Pow(k, 3) + 3)
