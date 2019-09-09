b54fd2701b79ce761b9b2e3ca26083a517811a8c
Oleksii Luppa <asleep.alex@gmail.com>
2019-09-09 11:19:52 +0300

    Added lb for 09.09

---
+++ 09.09_CSharp/09.09 CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09._09_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
