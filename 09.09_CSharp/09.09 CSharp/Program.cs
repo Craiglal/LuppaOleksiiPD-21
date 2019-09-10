7f75b7f68e5b90b95399e7d12972fd697f0248c0
Oleksii Luppa <asleep.alex@gmail.com>
2019-09-10 10:50:13 +0300

    Edit the 1 lab

--- 09.09_CSharp/09.09 CSharp/Program.cs
+++ 09.09_CSharp/09.09 CSharp/Program.cs
@@ -7,15 +7,23 @@ namespace _09._09_CSharp
         static void Main(string[] args)
         {
             double mult = 1.0;
+            int nn, nk;
+            bool a, b;
 
-            for (int k = 0; k < 50; k++)
+            do
             {
-                mult *= (((-1) ^ k * k ^ 2 + (-1) ^ (k ^ (2 + 1)) * k) * (2 * k ^ 3 + 6 * k ^ 2 + 6 * k + 5)) / ((2 * k ^ 3 + 3) * (-1) ^ (k + 1) * (k ^ 2 + 2 * k + 1) + (-1) ^ (k ^ 2 + 2) * (k + 1));
+                a = int.TryParse(Console.ReadLine(), out nn);
+                b = int.TryParse(Console.ReadLine(), out nk);
+                if (!(a & b) || (nn < 0 || nk < nn))
+                    Console.WriteLine("Error");
+            } while (!(a & b) || (nn < 0 || nk < nn));
+
+            for (int k = nn; k < nk; k++)
+            {
+                mult *= (Math.Pow(-1, k) * Math.Pow(k, 2) + Math.Pow(-1, Math.Pow(k, 2) + 1) * k) / (2 * Math.Pow(k, 3) + 3);
             }
 
             Console.WriteLine(mult);
         }
     }
 }
-// (((-1) ^ k * k ^ 2 + (-1) ^ (k ^ (2 + 1)) * k) * (2 * k ^ 3 + 6 * k ^ 2 + 6 * k + 5)) / ((2 * k ^ 3 + 3) * (-1) ^ (k + 1) * (k ^ 2 + 2 * k + 1) + (-1) ^ (k ^ 2 + 2) * (k + 1))
-// (Math.Pow(-1, k) * Math.Pow(k, 2) + Math.Pow(-1, Math.Pow(k, 2) + 1) * k) / (2 * Math.Pow(k, 3) + 3)
