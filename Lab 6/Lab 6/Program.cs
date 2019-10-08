35e7b187b63cfc3420d203bfb1894044aecf5440
Oleksii Luppa <asleep.alex@gmail.com>
2019-10-08 10:52:39 +0300

    Editted lab 6

--- Lab 6/Lab 6/Program.cs
+++ Lab 6/Lab 6/Program.cs
@@ -8,33 +8,25 @@ namespace Lab_6
     {
         static void Main(string[] args)
         {
-            int M, ch = 0, str = 0, intt = 0, dbl = 0, bl = 0;
-            bool a;
+            int ch = 0, str = 0, intt = 0, dbl = 0, bl = 0;
             List<object> list = new List<object>();
+            string inp;
 
             do
             {
-                Console.Write("Enter the number of objects: ");
-                a = int.TryParse(Console.ReadLine(), out M);
-                if (M <= 0 || !a)
-                    Console.WriteLine("ERROR!");
-            } while (M <= 0 || !a);
-
-            for (int i = 0; i < M; i++)
-            {
                 Console.Write("Enter the object: ");
-                string inp = Console.ReadLine();
+                inp = Console.ReadLine();
 
                 if (int.TryParse(inp, out int inputInt))
                 {
                     list.Add(inputInt);
                     intt++;
                 }
-                else if(double.TryParse(inp, out double inputDbl))
+                else if (double.TryParse(inp, out double inputDbl))
                 {
                     list.Add(inputDbl);
                     dbl++;
-                }  
+                }
                 else if (char.TryParse(inp, out char inputCh))
                 {
                     list.Add(inputCh);
@@ -50,7 +42,7 @@ namespace Lab_6
                     list.Add(inp);
                     str++;
                 }
-            }
+            } while (inp != "STOP");
 
             Console.WriteLine($"Char = {ch}, string = {str}, int = {intt}, double = {dbl}, bool = {bl}");
             Console.ReadKey();
