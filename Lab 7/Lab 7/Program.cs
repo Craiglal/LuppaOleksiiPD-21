bf600f8463a9446ae8d07b6cfa8b086419070fb7
Oleksii <asleep.alex@gmail.com>
2019-10-09 15:08:11 +0300

    Lab 7 task 1-4

--- Lab 7/Lab 7/Program.cs
+++ Lab 7/Lab 7/Program.cs
@@ -4,39 +4,114 @@ namespace Lab_7
 {
     class Program
     {
+        static int NumberOne(int input)
+        {
+            string result = "";
+            int counter = input.ToString().Length;
+
+            for (int i = 0; i < counter; i++)
+            {
+                int temp = input % 10;
+                input = input / 10;
+                result += temp;
+            }
+
+            int.TryParse(result, out int output);
+
+            return output;
+        }
+
+        static string StrOne(string input)
+        {
+            string output = "";
+            int counter = input.Length;
+
+            for (int i = counter - 1; i >= 0; i--)
+            {
+                output += input[i];
+            }
+
+            return output;
+        }
+
+        static double NumberTwo(double input)
+        {
+            string result = "";
+            string[] temp = input.ToString().Split('.', ',');
+
+            for (int i = temp[0].Length - 1; i >= 0; i--)
+            {
+                result += temp[0][i];
+            }
+
+            result += ",";
+
+            for (int i = temp[1].Length - 1; i >= 0; i--)
+            {
+                result += temp[1][i];
+            }
+
+            double.TryParse(result, out double output);
+
+            return output;
+        }
+
+        static string StrTwo(string input)
+        {
+            string output = "";
+            string[] temp = input.ToString().Split(',');
+            for (int i = 0; i < temp.Length; i++)
+            {
+                for (int j = temp[i].Length - 1; j >= 0; j--)
+                {
+                    output += temp[i][j];
+                    
+                }
+                if (temp.Length != i + 1)
+                    output += ',';
+            }
+
+            return output;
+        }
+
         static void Main(string[] args)
         {
-            int number;
+            int numberOne;                                  //Перше завдання,початок 
             bool a;
+
+            do                                             
+            {
+                Console.Write("Enter the number: ");
+                a = int.TryParse(Console.ReadLine(), out numberOne);
+                if (!a)
+                    Console.WriteLine("Wrong number!");
+            } while (!a);
+
+            Console.WriteLine(NumberOne(numberOne));         //Перше завдання, кінець   
+
+
+            string str = Console.ReadLine();              //Друге завдання, початок 
+
+            Console.WriteLine(StrOne(str));                  //Друге завдання, кінець 
+
+
+            double numberTwo;                               //Третє завдання, початок 
 
             do
             {
                 Console.Write("Enter the number: ");
-                a = int.TryParse(Console.ReadLine(), out number);
-                if (number <= 0 || !a)
+                a = double.TryParse(Console.ReadLine(), out numberTwo);
+                if (!a)
                     Console.WriteLine("Wrong number!");
-            } while (number <= 0 || !a);
+            } while (!a);
 
-            Console.WriteLine(number);
-            Console.WriteLine();
+            Console.WriteLine(NumberTwo(numberTwo));        //Третє завдання, кінець 
 
-            int counter = number.ToString().Length;
-            for (int i = 0; i < counter; i++)
-            {
-                int temp = number % 10;
-                number = number / 10;
-                Console.Write(temp);
-            }
-            Console.WriteLine();
 
-            string str = Console.ReadLine();
+            str = Console.ReadLine();                       //Четверте завдання, початок 
 
-            counter = str.Length;
-            for (int i = counter - 1; i >= 0 ; i--)
-            {
-                Console.Write(str[i]);
-            }
-            Console.WriteLine();
+            Console.WriteLine(StrTwo(str));                 //Четверте завдання, кінець 
+
         }
     }
 }
