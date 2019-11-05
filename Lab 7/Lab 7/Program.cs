abe4db717f8f5c7bea86c845341f12fa6976e2a7
Oleksii Luppa <asleep.alex@gmail.com>
2019-11-05 11:06:33 +0300

    Edit

--- Lab 7/Lab 7/Program.cs
+++ Lab 7/Lab 7/Program.cs
@@ -4,9 +4,9 @@ namespace Lab_7
 {
     class Program
     {
-        static void Reverse(int input)
-        {
-            string result = "";
+        static void Reverse(int input)
+        {
+            string result = "";
             int counter = input.ToString().Length;
 
             for (int i = 0; i < counter; i++)
@@ -14,69 +14,69 @@ namespace Lab_7
                 int temp = input % 10;
                 input = input / 10;
                 result += temp;
-            }
-
-            int.TryParse(result, out int output);
-
-            Console.WriteLine(output);
+            }
+
+            int.TryParse(result, out int output);
+
+            Console.WriteLine(output);
         }
 
-        static void Reverse(string input)
-        {
-            string output = "";
+        static void Reverse(string input)
+        {
+            string output = "";
             int counter = input.Length;
 
             for (int i = counter - 1; i >= 0; i--)
             {
                 output += input[i];
-            }
-
-            Console.WriteLine(output);
+            }
+
+            Console.WriteLine(output);
         }
 
-        static void ReverseSplit(double input)
-        {
-            string result = "";
-            string[] temp = input.ToString().Split('.', ',');
-
-            for (int i = temp[0].Length - 1; i >= 0; i--)
-            {
-                result += temp[0][i];
-            }
-
-            result += ",";
-
-            for (int i = temp[1].Length - 1; i >= 0; i--)
-            {
-                result += temp[1][i];
-            }
-
-            double.TryParse(result, out double output);
-
-            Console.WriteLine(output);
-        }
-
-        static void ReverseSplit(string input)
-        {
-            string output = "";
-            string[] temp = input.ToString().Split(',');
-            for (int i = 0; i < temp.Length; i++)
-            {
-                for (int j = temp[i].Length - 1; j >= 0; j--)
-                {
-                    output += temp[i][j];
-
-                }
-                if (temp.Length != i + 1)
-                    output += ',';
-            }
-
-            Console.WriteLine(output);
-        }
-
+        static void ReverseSplit(double input)
+        {
+            string result = "";
+            string[] temp = input.ToString().Split('.', ',');
+
+            for (int i = temp[0].Length - 1; i >= 0; i--)
+            {
+                result += temp[0][i];
+            }
+
+            result += ",";
+
+            for (int i = temp[1].Length - 1; i >= 0; i--)
+            {
+                result += temp[1][i];
+            }
+
+            double.TryParse(result, out double output);
+
+            Console.WriteLine(output);
+        }
+
+        static void ReverseSplit(string input)
+        {
+            string output = "";
+            string[] temp = input.ToString().Split(',');
+            for (int i = 0; i < temp.Length; i++)
+            {
+                for (int j = temp[i].Length - 1; j >= 0; j--)
+                {
+                    output += temp[i][j];
+
+                }
+                if (temp.Length != i + 1)
+                    output += ',';
+            }
+
+            Console.WriteLine(output);
+        }
+
         static void Main(string[] args)
         {
-            int numberOne;
+            int numberOne;
             bool a;
 
             do
@@ -92,7 +92,7 @@ namespace Lab_7
             Reverse(str);
 
             double numberTwo;
-            do
+            do
             {
                 Console.Write("Enter the number: ");
                 a = double.TryParse(Console.ReadLine(), out numberTwo);
@@ -102,7 +102,7 @@ namespace Lab_7
             ReverseSplit(numberTwo);
 
             str = Console.ReadLine();
-            ReverseSplit(str);
+            ReverseSplit(str);
         }
     }
 }
