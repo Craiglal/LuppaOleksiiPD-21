c5f7b996bc876b223caf64c5f20dafb76b898c4b
Oleksii <asleep.alex@gmail.com>
2020-02-24 20:58:27 +0200

    calculator

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        float a, b;
+        bool sign = true;
+        int count;
+
+        private void calculate()
+        {
+            switch (count)
+            {
+                case 1:
+                    b = a + float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 2:
+                    b = a - float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 3:
+                    b = a * float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 4:
+                    b = a / float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ',';
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 2;
+            label1.Text = a.ToString() + "-";
+            sign = true;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 3;
+            label1.Text = a.ToString() + "*";
+            sign = true;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 4;
+            label1.Text = a.ToString() + "/";
+            sign = true;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            calculate();
+            label1.Text = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            int lenght = textBox1.Text.Length - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i = 0; i < lenght; i++)
+            {
+                textBox1.Text = textBox1.Text + text[i];
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 1;
+            label1.Text = a.ToString() + "+";
+            sign = true;
+        }
+    }
+}
---
+++ Game/Game/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    public delegate void Go(ref int Player2Pos);
+
+    class Game
+    {
+        public event Go Player1 = null;
+        public event Go Player2 = null;
+
+        public void Player1Event(ref int Player1Pos)
+        {
+            if (Player1 != null)
+                Player1.Invoke(ref Player1Pos);
+        }
+
+        public void Player2Event(ref int Player2Pos)
+        {
+            if (Player2 != null)
+                Player2.Invoke(ref Player2Pos);
+        }
+
+        public void Start(ref int Player1Pos, ref int Player2Pos)
+        {
+            bool run = true;
+            while (run)
+            {
+                ConsoleKeyInfo s = Console.ReadKey();
+                switch (s.KeyChar)
+                {
+                    case '1':
+                        Player1Event(ref Player1Pos);
+                        break;
+                    case '2':
+                        Player2Event(ref Player2Pos);
+                        break;
+                    case 'q':
+                        run = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static private void Player1_Handler(ref int Player1Pos)
+        {
+            if (Player1Pos > 100)
+            {
+                Console.Clear();
+                Console.WriteLine("Player 1 win!");
+            }
+            else
+            {
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                Console.Write(" ");
+                Console.SetCursorPosition(Player1Pos, 1);
+                Player1Pos += 1;
+                Console.Write("#");
+            }
+        }
+
+        static private void Player2_Handler(ref int Player2Pos)
+        {
+            if (Player2Pos > 100)
+            {
+                Console.Clear();
+                Console.WriteLine("Player 2 win!");
+            }
+            else
+            {
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                Console.Write(" ");
+                Console.SetCursorPosition(Player2Pos, 9);
+                Player2Pos += 1;
+                Console.Write("#");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int Player1Pos = 9;
+            int Player2Pos = 9;
+            string finish = "FINISH";
+
+            Console.WriteLine("        |");
+            Console.WriteLine("Player 1|");
+            Console.WriteLine("        |");
+            Console.WriteLine("        S");
+            Console.WriteLine("        T");
+            Console.WriteLine("        A");
+            Console.WriteLine("        R");
+            Console.WriteLine("        T");
+            Console.WriteLine("        |");
+            Console.WriteLine("Player 2|");
+            Console.WriteLine("        |");
+
+            for (int i = 0; i < 11; i++)
+            {
+                if (i > 1 && i < 8)
+                {
+                    Console.SetCursorPosition(102, i);
+                    Console.WriteLine(finish[i - 2]);
+                }
+                else
+                {
+                    Console.SetCursorPosition(102, i);
+                    Console.WriteLine("|");
+                }
+            }
+
+            Game game = new Game();
+
+            game.Player1 += new Go(Player1_Handler);
+            game.Player2 += Player2_Handler;
+
+            game.Start(ref Player1Pos, ref Player2Pos);
+        }
+    }
+}
