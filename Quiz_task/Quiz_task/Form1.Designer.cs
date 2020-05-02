9ea9ccf96d53caf83a2af0f3d2ccc189b89648a2
Oleksii <asleep.alex@gmail.com>
2020-05-02 20:52:49 +0300

    Quiz

--- Quiz_task/Quiz_task/Form1.Designer.cs
+++ Quiz_task/Quiz_task/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Нова конфігурація";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(231, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Стара конфігурація";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(122, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Кросворд";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(395, 115);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Quiz_task/Quiz_task/Form1.cs
+++ Quiz_task/Quiz_task/Form1.cs
@@ -16,5 +16,60 @@ namespace Quiz_task
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Hide();
+            button2.Hide();
+            label1.Hide();
+
+            AddConfig();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void AddConfig()
+        {
+            TextBox width = new TextBox()
+            {
+                Location = new System.Drawing.Point(12, 12),
+                Name = "width",
+                Size = new System.Drawing.Size(100, 22),
+                TabIndex = 0,
+            };
+            Controls.Add(width);
+            TextBox height = new TextBox()
+            {
+                Location = new System.Drawing.Point(12, 40),
+                Name = "height",
+                Size = new System.Drawing.Size(100, 22),
+                TabIndex = 0,
+            };
+            Controls.Add(height);
+
+            Label widthlbl = new Label()
+            {
+                AutoSize = true,
+                Location = new System.Drawing.Point(119, 16),
+                Name = "widthlbl",
+                Size = new System.Drawing.Size(59, 17),
+                TabIndex = 2,
+                Text = "Ширина",
+            };
+            Controls.Add(widthlbl);
+            Label heightlbl = new Label()
+            {
+                AutoSize = true,
+                Location = new System.Drawing.Point(119, 43),
+                Name = "widthlbl",
+                Size = new System.Drawing.Size(55, 17),
+                TabIndex = 2,
+                Text = "Висота",
+            };
+            Controls.Add(heightlbl);
+        }
     }
 }
